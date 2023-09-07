8a3e2b5a73ba24f710cac6b5ad4436e026455f40
Kamalpreet kaur <126082752+kkamal2003@users.noreply.github.com>
2023-09-07 20:25:13 +0530

    first commit

---
+++ Plot3.R
@@ -0,0 +1,23 @@
+# first, set the corect working directory and then copy the unzipped from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip file into this directory, then follow the below steps.
+
+# load dplyr package
+library(dplyr) 
+
+# get data
+dat <- tbl_df(read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = c("?","")))
+
+# create truncated dataset by subsetting specified dates
+data <- filter(dat, Date == "1/2/2007" | Date == "2/2/2007")
+
+# remove dat from environment to free up RAM
+rm(dat)
+
+# convert date and time to proper formats
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$timetemp <- paste(data$Date, data$Time)
+data$Time <- strptime(data$timetemp, format = "%Y-%m-%d %H:%M:%S")
+
+# create line plot across time of global active power
+png(file = "plot2.png", width = 480, height = 480)
+plot(x = data$Time, y = data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
