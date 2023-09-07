0f946286d47f4dede0930eedb701647b32ac0e9a
Kamalpreet kaur <126082752+kkamal2003@users.noreply.github.com>
2023-09-07 21:21:10 +0530

    plot3 and plot4 commit

--- Plot3.R
+++ Plot3.R
@@ -1,23 +1,17 @@
-# first, set the corect working directory and then copy the unzipped from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip file into this directory, then follow the below steps.
+dataFile <- "C:/Users/ASUS/OneDrive/Documents/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-# load dplyr package
-library(dplyr) 
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
-# get data
-dat <- tbl_df(read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = c("?","")))
-
-# create truncated dataset by subsetting specified dates
-data <- filter(dat, Date == "1/2/2007" | Date == "2/2/2007")
-
-# remove dat from environment to free up RAM
-rm(dat)
-
-# convert date and time to proper formats
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
-data$timetemp <- paste(data$Date, data$Time)
-data$Time <- strptime(data$timetemp, format = "%Y-%m-%d %H:%M:%S")
-
-# create line plot across time of global active power
-png(file = "plot2.png", width = 480, height = 480)
-plot(x = data$Time, y = data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
--- Plot4.R
+++ Plot4.R
@@ -1,32 +1,29 @@
-# first, set the corect working directory and then copy the unzipped from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip file into this directory, then follow the below steps.
+dataFile <- "C:/Users/ASUS/OneDrive/Documents/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-# load dplyr package
-        library(dplyr) 
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 
-# get data
-        dat <- tbl_df(read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = c("?","")))
 
-# create truncated dataset by subsetting specified dates
-        data <- filter(dat, Date == "1/2/2007" | Date == "2/2/2007")
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
 
-# remove dat from environment to free up RAM
-        rm(dat)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
-# convert date and time to proper formats
-        data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
-        data$timetemp <- paste(data$Date, data$Time)
-        data$Time <- strptime(data$timetemp, format = "%Y-%m-%d %H:%M:%S")
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
-# create panel of graphs
-        png(file = "plot4.png", width = 480, height = 480)
-        par(mfrow = c(2, 2))
-        with(data, {
-                plot(x = data$Time, y = data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
-                plot(x = data$Time, y = data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-                plot(x = data$Time, y = data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-                lines(x = data$Time, y = data$Sub_metering_2, type = "l", col = "red")
-                lines(x = data$Time, y = data$Sub_metering_3, type = "l", col = "blue")
-                legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-                plot(x = data$Time, y = data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
-        })
-        dev.off()
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
